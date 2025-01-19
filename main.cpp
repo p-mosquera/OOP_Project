2526eee81f3885d206e3b2af851ed7092794b6aa
p-mosquera <p.mosquera@student.pk.edu.pl>
2025-01-19 16:31:58 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,28 +8,89 @@
 using namespace std;
 
 int main() {
-    // Crear instancias de las clases
-    Subject math("Matemáticas", 6);
-    Professor prof("Juan", "Pérez", "123456", "EMP001", "Matemáticas", &math);
-    Student stud("Ana", "Gómez", "789012", "STD001", "Ingeniería");
-    Staff staff("Luis", "Martínez", "345678", "STF001", "Administrativo");
+    // Create class instances
+    Subject math("Mathematics", 6);
+    Professor prof("John", "Smith", "123456", "EMP001", "Mathematics", &math);
+    Student stud("Anna", "Jones", "789012", "STD001", "Engineering");
+    Staff staff("Louis", "Martin", "345678", "STF001", "Administrative");
 
-    // Describir a cada persona
+    // Describe each person
     prof.describePerson();
     stud.describePerson();
     staff.describePerson();
 
     University myUniversity("Example");
+    int option;
 
-    // Agregar miembros a la universidad
-    myUniversity.addMember(&prof);
-    myUniversity.addMember(&stud);
-    myUniversity.addMember(&staff);
+    do {
+        cout << "\nMain Menu:" << endl;
+        cout << "1. Add professor" << endl;
+        cout << "2. Add student" << endl;
+        cout << "3. Add staff" << endl;
+        cout << "4. Show university members" << endl;
+        cout << "5. Add subject" << endl;
+        cout << "0. Exit" << endl;
+        cout << "Select an option: ";
+        cin >> option;
 
-    // Describir todos los miembros de la universidad
-    myUniversity.describeMembers();
+        switch (option) {
+            case 1: {
+                string name, lastname, dni, empId, dept;
+                cout << "Name: "; cin >> name;
+                cout << "Last name: "; cin >> lastname;
+                cout << "ID number: "; cin >> dni;
+                cout << "Employee ID: "; cin >> empId;
+                cout << "Department: "; cin >> dept;
+                
+                Subject* subject = new Subject(dept, 6);
+                Professor* prof = new Professor(name, lastname, dni, empId, dept, subject);
+                myUniversity.addMember(prof);
+                break;
+            }
+            case 2: {
+                string name, lastname, dni, studId, career;
+                cout << "Name: "; cin >> name;
+                cout << "Last name: "; cin >> lastname;
+                cout << "ID number: "; cin >> dni;
+                cout << "Student ID: "; cin >> studId;
+                cout << "Major: "; cin >> career;
+                
+                Student* stud = new Student(name, lastname, dni, studId, career);
+                myUniversity.addMember(stud);
+                break;
+            }
+            case 3: {
+                string name, lastname, dni, staffId, role;
+                cout << "Name: "; cin >> name;
+                cout << "Last name: "; cin >> lastname;
+                cout << "ID number: "; cin >> dni;
+                cout << "Staff ID: "; cin >> staffId;
+                cout << "Role: "; cin >> role;
+                
+                Staff* staff = new Staff(name, lastname, dni, staffId, role);
+                myUniversity.addMember(staff);
+                break;
+            }
+            case 4:
+                myUniversity.describeMembers();
+                break;
+            case 5: {
+                string name;
+                int credits;
+                cout << "Subject name: "; cin >> name;
+                cout << "Credits: "; cin >> credits;
+                
+                Subject* subject = new Subject(name, credits);
+                cout << "Subject added successfully!" << endl;
+                break;
+            }
+            case 0:
+                cout << "Exiting program..." << endl;
+                break;
+            default:
+                cout << "Invalid option" << endl;
+        }
+    } while (option != 0);
 
     return 0;
 }
-
-
