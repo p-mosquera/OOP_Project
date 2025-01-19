ac6c98f7c4e0e542b589d5dbb9b90393b2bf17e4
p-mosquera <p.mosquera@student.pk.edu.pl>
2025-01-19 17:38:33 +0100

    Update subject.cpp

--- subject.cpp
+++ subject.cpp
@@ -1,10 +1,7 @@
 #include "subject.hpp"
 
 // Implementación del constructor
-Subject::Subject(const string& n, const int e) : name(n), ects(e), semester(0), teacher(nullptr) {}
-
-// Implementación del constructor que permite asignar un profesor
-Subject::Subject(const string& n, const int e, Professor* prof) : name(n), ects(e), semester(0), teacher(prof) {}
+Subject::Subject(const string& n, const int e) : name(n), ects(e), semester(0){}
 
 // Destructor virtual puro
 Subject::~Subject() {}
