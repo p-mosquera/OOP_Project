90164725ef370224f0f3d2187fcc95b5b222c023
p-mosquera <p.mosquera@student.pk.edu.pl>
2025-01-20 12:49:09 +0100

    Update staff.cpp

--- staff.cpp
+++ staff.cpp
@@ -1,12 +1,10 @@
 #include "staff.hpp"
 
-// Implementación del constructor virtual puro de la clase Person
-Person::~Person() {}
 
 // Método para describir al personal
 void Staff::describePerson() {
-    cout << "Nombre: " << getName() << ", Apellido: " << getSurname() 
-         << ", ID: " << getID() << ", Staff ID: " << staffID 
-         << ", Occupation: " << position << endl;
+    cout << "Nombre: " << getName() << ", Apellido: " << getSurname()
+         << ", ID: " << getID() << ", Employee ID: " << staffID
+         << ", Position: " << position << endl;
 }
 
