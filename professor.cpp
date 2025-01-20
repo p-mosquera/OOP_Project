9b3e6fe6b0278c076308601adf35e91d1ee87e21
p-mosquera <p.mosquera@student.pk.edu.pl>
2025-01-20 12:48:26 +0100

    Update professor.cpp

--- professor.cpp
+++ professor.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 #include "professor.hpp"
 
-// Implementación del constructor virtual puro de la clase Person
-Person::~Person() {}
 
 // Método para describir al profesor
 void Professor::describePerson() {
     cout << "Name: " << getName() << ", Surname: " << getSurname() 
          << ", ID: " << getID() << ", Employee ID: " << employeeID 
-         << ", Departament: " << department 
-         << ", Subject: " << (subject ? subject->getSubject() : "N/A") << endl;
+         << ", Departament: " << department << endl;
 }
