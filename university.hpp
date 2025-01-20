860e279f52f8acba6ac8693385cf9f9fce75e0db
p-mosquera <p.mosquera@student.pk.edu.pl>
2025-01-20 12:52:14 +0100

    Update university.hpp

--- university.hpp
+++ university.hpp
@@ -2,6 +2,7 @@
 #define UNIVERSITY_HPP
 
 #include <string>
+#include <iostream>
 #include <vector>
 #include "person.hpp"
 #include "subject.hpp"
@@ -19,31 +20,12 @@ public:
     University(const string& n) : name(n) {}
 
     // Destructor
-    ~University() {
-        for (auto member : members) {
-            delete member; // Liberar memoria de los miembros
-        }
-        for (auto subject : subjects) {
-            delete subject; // Liberar memoria de las asignaturas
-        }
-    }
+    ~University();
 
-    // Método para agregar un miembro
-    void addMember(Person* member) {
-        members.push_back(member);
-    }
-
-    // Método para agregar una asignatura
-    void addSubject(Subject* subject) {
-        subjects.push_back(subject);
-    }
-
-    // Método para describir todos los miembros
-    void describeMembers() const {
-        for (const auto& member : members) {
-            member->describePerson();
-        }
-    }
+    // Declaraciones de métodos
+    void addMember(Person* member);
+    void addSubject(Subject* subject);
+    void describeMembers() const;
 
     // Método para obtener el nombre de la universidad
     string getName() const {
