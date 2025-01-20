c89c33f5b8cf381fd1a80fc33feb9bb9dbe08984
p-mosquera <p.mosquera@student.pk.edu.pl>
2025-01-20 12:50:40 +0100

    Update student.cpp

--- student.cpp
+++ student.cpp
@@ -1,11 +1,9 @@
 #include "student.hpp"
 
-// Implementación del constructor virtual puro de la clase Person
-Person::~Person() {}
 
 // Método para describir al estudiante
 void Student::describePerson() {
-    cout << "Nombre: " << getName() << ", Apellido: " << getSurname() 
+    cout << "Name: " << getName() << ", Surname: " << getSurname() 
          << ", ID: " << getID() << ", Student ID: " << studentID 
-         << ", Carrera: " << major << endl;
+         << ", Major: " << major << endl;
 }
