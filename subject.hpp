09e1f7c1ebb980b8a4eb5663e47c29db3a698bc8
p-mosquera <p.mosquera@student.pk.edu.pl>
2025-01-20 12:51:49 +0100

    Update subject.hpp

--- subject.hpp
+++ subject.hpp
@@ -2,25 +2,34 @@
 #define SUBJECT_HPP
 
 #include <string>
+#include <iostream>
+#include "professor.hpp" // Asegúrate de que este archivo esté correctamente definido
 
 using namespace std;
 
-class Subject{
-    private:
-        string name;
-        int ects, semester;
+class Subject {
+private:
+    string name;
+    int ects, semester;
+    Professor* teacher; // Asegúrate de que la clase Professor esté definida
+
+public:
+    // Constructor
+    Subject(const string& n, const int e);
     
+    // Constructor que permite asignar un profesor
+    Subject(const string& n, const int e, Professor* prof);
 
-    public:
-        //constructor
-        Subject(const string& n, const int e);
+    // Destructor
+    virtual ~Subject(); // Destructor virtual
 
-        //virtual destructor
-        virtual ~Subject() = 0;
+    // Getters
+    string getSubject() const;
 
-        //getters & setters
-        string getSubject() const;
-
-
+    // Método para asignar un profesor a la asignatura
+    void assignProfessor(Professor* prof) {
+        teacher = prof; // Asigna el profesor a la asignatura
+    }
 };
-#endif //subject_hpp
+
+#endif // SUBJECT_HPP
