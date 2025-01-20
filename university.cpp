2d681ff91dadb531b62eee9fb2c1021d708477d0
p-mosquera <p.mosquera@student.pk.edu.pl>
2025-01-20 15:41:21 +0100

    Update university.cpp

--- university.cpp
+++ university.cpp
@@ -19,9 +19,23 @@ void University::describeMembers() const {
     }
 }
 
+// Método para comprobar existencia de miembros
+bool University::personExists(const string& dni) {
+    for (const auto& member : members) {
+        if (member->getID() == dni) { // Asumiendo que getID() devuelve el DNI
+            return true; // La persona ya existe
+        }
+    }
+    return false; // La persona no existe
+}
+
 // Método para agregar un miembro
-void University::addMember(Person* member) {
-    members.push_back(member);
+void University::addMember(Person* person) {
+    if (!personExists(person->getID())) { // Verifica si la persona ya existe
+        members.push_back(person); // Agrega el miembro si no existe
+    } else {
+        cout << "This person already exists." << endl;
+    }
 }
 
 // Método para agregar una asignatura
